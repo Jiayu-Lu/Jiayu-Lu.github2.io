9fcafc058181ef3d550ca3cb7a8f1d7d3e04a81e
Jiayu Lu <84060030+Jiayu-Lu@users.noreply.github.com>
2021-06-03 05:32:25 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,6 @@
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
-
-document.querySelector('img').onclick = function() {
-    alert('别戳我，我怕疼。');
-}
-
 let myImage = document.querySelector('img');
-
 myImage.onclick = function() {
+    alert('别戳我，我怕疼。');
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/image1.jpg') {
       myImage.setAttribute('src', 'images/image2.jpg');
@@ -18,11 +11,14 @@ myImage.onclick = function() {
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
-
 function setUserName() {
   let myName = prompt('请输入你的名字。');
   localStorage.setItem('name', myName);
   myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+}
+
+myButton.onclick = function() {
+   setUserName();
 }
 
 if(!localStorage.getItem('name')) {
@@ -32,6 +28,3 @@ if(!localStorage.getItem('name')) {
   myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
 }
 
-myButton.onclick = function() {
-   setUserName();
-}
