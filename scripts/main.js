0cfbd43052a9846a4a8ac0b02742b94b58c6fd36
Jiayu Lu <84060030+Jiayu-Lu@users.noreply.github.com>
2021-06-03 05:17:04 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,17 @@
 let myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 
-document.querySelector('html').onclick = function() {
+document.querySelector('body').onclick = function() {
     alert('别戳我，我怕疼。');
+}
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/image1.jpg') {
+      myImage.setAttribute('src', 'images/image2.jpg');
+    } else {
+      myImage.setAttribute('src', 'images/image1.jpg');
+    }
 }
